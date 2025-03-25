d3f4eed020ed9ea5131ac7caaa3a0b62eeb4bb0c
aazaadcoder <128458378+aazaadcoder@users.noreply.github.com>
2025-03-25 21:10:57 +0530

    add the member model but it needs to be updated when role model in finished

---
+++ backend/src/models/member.model.ts
@@ -0,0 +1,33 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+
+
+export interface MemberDocument extends Document {
+    userId: mongoose.Types.ObjectId;
+    workspaceId: mongoose.Types.ObjectId;
+    // role: RoleDocument 
+    joinedAt: Date,
+}
+
+const memberSchema = new Schema<MemberDocument>({
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    // role: {
+    //     type: Schema.Types.ObjectId,
+    //     ref: "Role",
+    //     required: true,
+    // },
+    workspaceId: {
+        type: Schema.Types.ObjectId,
+        ref: "WorkSpace",
+        required: true,
+    },
+    joineAt: {
+        type: Date,
+        required: true
+    }
+
+})
