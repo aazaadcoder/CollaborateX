5938e6c10584436cf1972c6113874833e13e4a8f
aazaadcoder <128458378+aazaadcoder@users.noreply.github.com>
2025-03-26 11:05:02 +0530

    add the role-permission model using the role, persmission types and object and update the ,members model with this new role-permission model integration and also add rules for role and its permssion in permision-rule.enum.ts

--- backend/src/models/member.model.ts
+++ backend/src/models/member.model.ts
@@ -1,11 +1,12 @@
 import mongoose, { Document, Schema } from "mongoose";
+import { RoleDocument } from "./role-permission.model";
 
 
 
 export interface MemberDocument extends Document {
     userId: mongoose.Types.ObjectId;
     workspaceId: mongoose.Types.ObjectId;
-    // role: RoleDocument 
+    role: RoleDocument 
     joinedAt: Date,
 }
 
@@ -15,11 +16,11 @@ const memberSchema = new Schema<MemberDocument>({
         ref: "User",
         required: true,
     },
-    // role: {
-    //     type: Schema.Types.ObjectId,
-    //     ref: "Role",
-    //     required: true,
-    // },
+    role: {
+        type: Schema.Types.ObjectId,
+        ref: "Role",
+        required: true,
+    },
     workspaceId: {
         type: Schema.Types.ObjectId,
         ref: "WorkSpace",
--- backend/src/models/role-permission.model.ts
+++ backend/src/models/role-permission.model.ts
@@ -1,3 +1,35 @@
+import { RoleDocument } from './role-permission.model';
+import { Permissions, PermissionType, Roles } from './../enums/role.enum';
+import { RoleType } from "../enums/role.enum";
+import { Schema } from 'mongoose';
+import { object } from 'zod';
+import { RolePermission } from '../utils/role-persmission';
 
 
-export interface RoleDocument extan
+export interface RoleDocument extends Document {
+    name: RoleType,
+    permissions: Array<PermissionType>;
+}
+
+const roleSchema = new Schema<RoleDocument>(
+    {
+        name: {
+            type: String,
+            enum: Object.values(Roles),
+            required : true,
+            unique : true,
+            
+        },
+        permissions: {
+            type : [String],
+            enum : Object.values(Permissions),
+            required : true,
+            default : function (this: RoleDocument) {
+                return RolePermission[this.name]
+            }
+        }
+    },
+    {
+        timestamps: true
+    }
+)
