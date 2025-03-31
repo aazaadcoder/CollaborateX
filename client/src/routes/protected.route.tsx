89858618a8124420f3757437a44365179858b0bd
aazaadcoder <128458378+aazaadcoder@users.noreply.github.com>
2025-03-31 19:38:09 +0530

    change protected routes to only allow autheticated users to access them and redirect unauth users to login page

--- client/src/routes/protected.route.tsx
+++ client/src/routes/protected.route.tsx
@@ -1,6 +1,21 @@
-import { Outlet } from "react-router-dom";
+import { DashboardSkeleton } from "@/components/skeleton-loaders/dashboard-skeleton";
+import useAuth from "@/hooks/api/use-auth";
+import { Navigate, Outlet } from "react-router-dom";
 const ProtectedRoute = () => {
-  return <Outlet />;
+  // 1. we will check if user is authenticated or not 
+  // 2. if yes then we will allow it access the protected children routes 
+  // 3. otherwise redirect to login page
+
+  const {isPending , data : authData} = useAuth();
+  const user = authData?.user;
+
+  if(isPending) return <DashboardSkeleton/>
+
+  if(!user) return <Navigate to="/" replace/>
+  // 1. by using replace, the current route will be replaced by the navigate route 
+  // 2. now we cannot access the current route after navigation
+
+ return <Outlet />;
 };
 
 export default ProtectedRoute;
